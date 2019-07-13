02f5cc40cafed6c324dd6748c0cb3927593b6807
destbg <35627380+destbg@users.noreply.github.com>
2019-07-13 14:57:45 +0300

    Product Page

--- src/Presentation/GardenMarket.Web/Controllers/HomeController.cs
+++ src/Presentation/GardenMarket.Web/Controllers/HomeController.cs
@@ -5,17 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GardenMarket.Web.Models;
+using GardenMarket.Service.Interface;
 
 namespace GardenMarket.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductService _product;
+
+        public HomeController(IProductService product)
+        {
+            _product = product ?? throw new ArgumentNullException(nameof(product));
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Product()
         {
             return View();
         }
